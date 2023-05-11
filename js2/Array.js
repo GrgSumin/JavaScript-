639fcc6cdcfa9e85bda30b8ef82194397d00ff24
GrgSumin <be2019se673@gces.edu.np>
2023-05-11 17:18:42 +0545

    Add files via upload

---
+++ js2/Array.js
@@ -0,0 +1,6 @@
+// Array is a variable where you can store multiple values
+let roll_no = [1,2,3,4]
+let name = ["hari", "Ram", "Shyam", "Raju"]
+for(let i =0; i<roll_no.length; i++){
+    console.log(`${name[i]} roll number is  ${roll_no[i]}`)
+}
